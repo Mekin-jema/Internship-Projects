98754b0963164d4d8f95378690d7d8085c3e40dc
Mekin Jemal <mekinjemal999@gmail.com>
2024-05-06 15:39:50 +0300

    The CSS decoration is finished

---
+++ PRODIGY_TrackCode_TaskNumber/Prodigy-Task-02/main.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapseTime = 0;
+let isRunning = false;
+
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - eplapseTime;
+  }
+}
+function Stop() {}
+function Reset() {}
