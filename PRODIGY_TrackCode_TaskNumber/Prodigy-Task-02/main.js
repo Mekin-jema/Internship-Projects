55338f4c75f271206c7041b0fd0e4b8b7f537b84
Mekin Jemal <mekinjemal999@gmail.com>
2024-05-06 18:04:50 +0300

    Here is  the final project of stop watch

--- PRODIGY_TrackCode_TaskNumber/Prodigy-Task-02/main.js
+++ PRODIGY_TrackCode_TaskNumber/Prodigy-Task-02/main.js
@@ -1,4 +1,4 @@
-const display = document.getElementById("display");
+const display = document.querySelector(".display");
 let timer = null;
 let startTime = 0;
 let elapseTime = 0;
@@ -6,8 +6,38 @@ let isRunning = false;
 
 function start() {
   if (!isRunning) {
-    startTime = Date.now() - eplapseTime;
+    startTime = Date.now() - elapseTime;
+    timer = setInterval(update, 1);
+    isRunning = true;
   }
 }
-function Stop() {}
-function Reset() {}
+
+function stops() {
+  if (isRunning) {
+    clearInterval(timer);
+    elapseTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
+function reset() {
+  clearInterval(timer);
+  startTime = 0;
+  elapseTime = 0;
+  isRunning = false;
+  display.textContent = "00:00:00:00";
+}
+
+function update() {
+  const currentTime = Date.now();
+  elapseTime = currentTime - startTime;
+  let hours = Math.floor(elapseTime / (1000 * 60 * 60));
+  let minutes = Math.floor((elapseTime / (1000 * 60)) % 60);
+  let seconds = Math.floor((elapseTime / 1000) % 60);
+  let miliseconds = Math.floor((elapseTime % 1000) / 10);
+  // console.log(seconds);
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  miliseconds = String(miliseconds).padStart(2, "0");
+  display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
